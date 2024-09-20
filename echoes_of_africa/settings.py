48e588f46ed9159810775ab1bbe3efe0cc7c9008
Abdulkadir Abubakar <abdulkaadiras@gmail.com>
2024-09-20 07:42:36 +0100

    Adjusted settings.py

--- echoes_of_africa/settings.py
+++ echoes_of_africa/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['echoes-of-africa.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['echoes-of-africa.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
