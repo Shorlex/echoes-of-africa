887d78e4edf0c4fb29d03edb414e0199df08d8b1
Abdulkadir Abubakar <abdulkaadiras@gmail.com>
2024-09-20 10:49:35 +0100

    Another settings.py adjustment

--- echoes_of_africa/settings.py
+++ echoes_of_africa/settings.py
@@ -95,10 +95,7 @@ DATABASES = {
         # 'PASSWORD': env('DB_PASSWORD'),
         # 'HOST': env('DB_HOST'),
         # 'PORT': env('DB_PORT'),
-        dj_database_url.config(
-        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
-        conn_max_age=600
-    )
+        dj_database_url.config(conn_max_age=600)
     
 }
 
